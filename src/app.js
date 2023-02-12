c9c2822bb3c2fca172e107c34fb39170bd7180d4
Marcos Moraes <contato@marcosamoraes.com>
2023-02-11 22:22:24 -0300

    project started

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import router from './routes/routes.js';
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(router);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ src/schemas/validator.js
@@ -0,0 +1,6 @@
+const validator = (schema) => (payload) => {
+  const result = schema.validate(payload, { abortEarly: false });
+  return result.error ? { error: result.error.details[0].message } : payload;
+};
+
+export default validator;
