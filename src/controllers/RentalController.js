647480395ce0f00723cff2f092278ea898060597
Marcos Moraes <contato@marcosamoraes.com>
2023-02-13 01:44:30 -0300

    rentals insert and list

--- src/controllers/RentalController.js
+++ src/controllers/RentalController.js
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-destructuring */
+import dayjs from 'dayjs';
 import validateStoreRentalsSchema from '../schemas/storeRentalsSchema.js';
 import validateUpdateRentalsSchema from '../schemas/updateRentalsSchema.js';
 import validateFinishRentalsSchema from '../schemas/finishRentalsSchema.js';
@@ -5,7 +7,15 @@ import db from '../config/database.js';
 
 export async function list(req, res) {
   try {
-    const rentals = await db.query('SELECT * FROM rentals');
+    const rentals = await db.query(`
+      SELECT
+        r.*,
+        JSON_BUILD_OBJECT('id', c.id, 'name', c.name) AS customer,
+        JSON_BUILD_OBJECT('id', g.id, 'name', g.name) AS game
+      FROM rentals r
+      JOIN customers c ON r."customerId" = c."id"
+      JOIN games g ON r."gameId" = g."id"
+    `);
     return res.send(rentals.rows);
   } catch (err) {
     return res.status(500).send(err);
@@ -14,12 +24,34 @@ export async function list(req, res) {
 
 export async function store(req, res) {
   const {
+    customerId,
+    gameId,
+    daysRented,
     error,
   } = await validateStoreRentalsSchema(req.body);
 
   if (error) return res.status(error.code).send(error.message);
 
   try {
+    let customer = await db.query('SELECT * FROM customers WHERE "id" = $1', [customerId]);
+    customer = customer.rows[0];
+    if (!customer) return res.sendStatus(409);
+
+    let game = await db.query('SELECT * FROM games WHERE "id" = $1', [gameId]);
+    game = game.rows[0];
+    if (!game) return res.sendStatus(409);
+
+    const quantityGameRented = await db.query('SELECT count(id) as quantity FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL', [game.id]);
+
+    if (game.stockTotal <= quantityGameRented.rows[0].quantity) return res.sendStatus(400);
+
+    const originalPrice = daysRented * game.pricePerDay;
+
+    await db.query(
+      'INSERT INTO rentals ("customerId", "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") VALUES ($1, $2, $3, $4, null, $5, null)',
+      [customerId, gameId, dayjs().format(), daysRented, originalPrice],
+    );
+
     return res.sendStatus(201);
   } catch (err) {
     return res.status(500).send(err);
--- src/schemas/storeRentalsSchema.js
+++ src/schemas/storeRentalsSchema.js
@@ -2,8 +2,9 @@ import Joi from 'joi';
 import validator from './validator.js';
 
 const storeRentalsSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  customerId: Joi.number().required(),
+  gameId: Joi.number().required(),
+  daysRented: Joi.number().min(1).required(),
 });
 
 const validateStoreRentalsSchema = validator(storeRentalsSchema);
